6be0ebce058a0b393ceae7a97549f3bd1dd08236
ptoniev <p.ivanov@proxiad.com>
2022-03-17 09:31:50 +0200

    Refactored servlet

--- src/main/java/bg/hangman/practice/ServletGame.java
+++ src/main/java/bg/hangman/practice/ServletGame.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/game/*")
-public class ServletPresentation extends HttpServlet {
+public class ServletGame extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
